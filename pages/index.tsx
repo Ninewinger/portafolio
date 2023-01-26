deed0beacb1f0117f5f383b5e6e838bf4b885dce
Diego Vergara <71197696+Ninewinger@users.noreply.github.com>
2023-01-26 19:38:49 -0300

    just added some text to home

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,20 +14,18 @@ export default function Home() {
           <div className="flex flex-col-reverse sm:flex-row items-start">
             <div className="flex flex-col pr-8">
               <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-1 text-black dark:text-white">
-                Lee Robinson
+                Diego Vergara Petricio
               </h1>
               <h2 className="text-gray-700 dark:text-gray-200 mb-4">
-                VP of Developer Experience at{' '}
-                <span className="font-semibold">Vercel</span>
+                Full-Stack Developer, Gamer and Machine Learning Enthusiast
               </h2>
-              <p className="text-gray-600 dark:text-gray-400 mb-16">
-                Helping developers build a faster web. Teaching about web
-                development, serverless, and React / Next.js.
+              <p className="text-gray-600 dark:text-gray-400 mb-16 pr-5">
+              I am extremely excited about my career in web development and I am constantly looking for new ways to learn and improve. I am passionate about using my skills to create beautiful and functional websites that make a difference. I am also passionate about machine learning, and I am always looking for ways to incorporate it into my projects to make them more interactive and intelligent. I am confident that my enthusiasm and eagerness to learn will help me to excel in this field and make a positive impact in the world of technology.
               </p>
             </div>
-            <div className="w-[80px] sm:w-[176px] relative mb-8 sm:mb-0 mr-auto">
+            <div className="flex-shrink-0">
               <Image
-                alt="Lee Robinson"
+                alt="Diego Antonio Vergara Petricio"
                 height={176}
                 width={176}
                 src="/avatar.jpg"
