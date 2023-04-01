4d4e8f8be86f3f97d05b0843cb4f9fa461770664
Diego Vergara <71197696+Ninewinger@users.noreply.github.com>
2023-03-31 21:36:16 -0300

    agregados campos de card y lista de objetos para portafolio

---
+++ components/PortafolioCard.tsx
@@ -0,0 +1,41 @@
+export default function PortafolioCard({ title, description, tecnologies, image, link }) {
+  return (
+    <div className="bg-gray-200 dark:bg-gray-800 rounded-lg shadow-md overflow-hidden">
+      <div className="p-4">
+        <h2 className="text-xl font-bold text-gray-800 dark:text-white">
+            {title}
+        </h2>
+        <p className="mt-2 text-gray-600 dark:text-gray-400">
+            {description}
+        </p>
+        <p className="mt-2 text-gray-600 dark:text-gray-400 font-bold">
+            Tecnologies:{tecnologies}
+        </p>
+      </div>
+      <img
+        className="h-56 w-full object-contain"
+        src={image}
+        alt="avatar"
+      />
+      <div className="flex items-center justify-between p-4 bg-gray-200 dark:bg-gray-700">
+        <a
+          href={link}
+          target="_blank"
+          className="text-blue-500 hover:underline"
+        >
+          Read more
+        </a>
+        <div className="flex items-center">
+          <img
+            className="h-8 w-8 rounded-full object-cover"
+            src="/avatar.jpg"
+            alt="avatar"
+          />
+          <h4 className="ml-2 text-gray-700 dark:text-gray-200 font-semibold">
+            Diego Vergara
+          </h4>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,6 +1,37 @@
 import Container from 'components/Container';
+import PortafolioCard from 'components/PortafolioCard';
 
 export default function portafolio() {
+    let props = {
+        firstProject: {
+            title: "Music player with fetch",
+            description: "This project is a music player that uses an 8bit music API to obtain the data of the artists and songs.",
+            tecnologies: "HTML, CSS, JavaScript, React, fetch.",
+            image: "https://raw.githubusercontent.com/Ninewinger/music_player_with_fetch/master/public/musicplayerreadme.jpg",
+            link: "https://github.com/Ninewinger/music_player_with_fetch"
+        },
+        secondProject: {
+            title: "Calculator",
+            description: "This project is a calculator that uses Tkinter to create the GUI. and a calculator made only on oop.",
+            tecnologies: "Python, Tkinter, OOP.",
+            image: "https://raw.githubusercontent.com/Ninewinger/calculadora/master/calc.bmp",
+            link: "https://github.com/Ninewinger/calculadora"
+        },
+        thirdProject: {
+            title: "BATTLESHIP",
+            description: "This project is a battleship game made in vanilla js, it has a simple UI and a simple AI.",
+            tecnologies: "HTML, CSS, JavaScript.",
+            image: "https://raw.githubusercontent.com/Ninewinger/Battleship-In-Vanilla.js/main/readmeimg.jpg",
+            link: "https://github.com/Ninewinger/Battleship-In-Vanilla.js"
+        },
+        fourthProject: {
+            title: "Random Card Generator",
+            description: "This project is a random card generator with the select algorithm.",
+            tecnologies: "HTML, CSS, JavaScript.",
+            image: "https://raw.githubusercontent.com/Ninewinger/Sorting_Cards_using_the_selection_algorithm/master/asd.jpg",
+            link: "https://github.com/Ninewinger/Sorting_Cards_using_the_selection_algorithm/tree/master"
+        }
+    }
     return (
         <Container 
             title="Portafolio – Diego Vergara Petricio"
@@ -147,6 +178,9 @@ export default function portafolio() {
                 <h3 className="text-2xl font-semibold text-gray-700 dark:text-gray-200 pb-12">
                     Machine-Learning projects, data science, and more.
                 </h3>
+                <p className="text-gray-600 dark:text-gray-400 pb-12">
+                    I'm currently learning machine learning and data science, here are some of my projects.
+                </p>
                 
             </div>
         </Container>
