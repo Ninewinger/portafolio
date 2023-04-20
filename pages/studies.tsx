778e2ce67a526f72dd664e79b575c231bf38c2b0
Diego Vergara <71197696+Ninewinger@users.noreply.github.com>
2023-04-19 21:24:42 -0400

    added alt to img

--- pages/studies.tsx
+++ pages/studies.tsx
@@ -18,6 +18,7 @@ export default function studies() {
             <img
               className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
               src="/prev.jpg"
+              alt=""
             ></img>
             <div className="flex flex-col justify-between p-4 leading-normal">
               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
@@ -39,6 +40,7 @@ export default function studies() {
             <img
               className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
               src="/full_stack_banner.avif"
+              alt=""
             ></img>
             <div className="flex flex-col justify-between p-4 leading-normal">
               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
